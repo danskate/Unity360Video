939b5564a28b631ce5d070474f8aeefebb3216af
dannygames <dangeislerjr@gmail.com>
2023-08-23 20:08:35 -0700

    Add VR Rig

--- Assets/VideoController.cs
+++ Assets/VideoController.cs
@@ -9,14 +9,15 @@ public class VideoController : MonoBehaviour
     public Material skyboxMat;
     private RenderTexture texture;
     private VideoPlayer videoPlayer;
+
     // Start is called before the first frame update
     void Awake()
     {
         videoPlayer = GetComponent<VideoPlayer>();
-        texture = new RenderTexture(7680, 3840,24);
+        texture = new RenderTexture(4096,2160,32, RenderTextureFormat.ARGB32);
         videoPlayer.targetTexture = texture;
         skyboxMat.mainTexture = texture;
-
+        
 
     }
 
